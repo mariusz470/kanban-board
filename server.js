640b3c2e6f4eaf36a3724f38628a108f0fcd792f
Mariusz Kiciński <mariusz470@users.noreply.github.com>
2021-02-28 19:26:16 +0100

    Version 1.0.0

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -15,6 +16,15 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
+
+if (process.env.NODE_ENV === "production") {
+  // Set Static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
